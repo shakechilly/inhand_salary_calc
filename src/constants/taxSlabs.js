b626c9b571d1cbb4d63867087318827b491afe40
Abhishek Pandey <shakechilly@protonmail.com>
2026-06-12 16:46:48 +0530

    Delete taxSlabs.js

--- src/constants/taxSlabs.js
+++
@@ -1,9 +0,0 @@
-export const SLABS = [
-  { min: 0, max: 400000, rate: 0.00 },
-  { min: 400000, max: 800000, rate: 0.05 },
-  { min: 800000, max: 1200000, rate: 0.10 },
-  { min: 1200000, max: 1600000, rate: 0.15 },
-  { min: 1600000, max: 2000000, rate: 0.20 },
-  { min: 2000000, max: 2400000, rate: 0.25 },
-  { min: 2400000, max: Infinity, rate: 0.30 }
-];
