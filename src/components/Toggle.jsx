62c0641cfdbe05489b0814959af1ec6f3443db9f
Abhishek Pandey <shakechilly@protonmail.com>
2026-06-12 16:48:08 +0530

    Delete Toggle.jsx

--- src/components/Toggle.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Toggle({ checked, onChange, label }) {
-  return (
-    <button onClick={() => onChange(!checked)}>
-      {label}: {checked ? "ON" : "OFF"}
-    </button>
-  );
-}
